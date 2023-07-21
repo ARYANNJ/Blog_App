99eba0747460eb500a6792df4ed89cc0aa2441d5
ARYANNJ <aryanjadhav71@gmail.com>
2023-07-21 20:17:31 +0530

    successfully connected user to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./routes/user.routes";
 const app=express();
+app.use(express.json());
 
+app.use("/api/user",router);
 mongoose.connect("mongodb+srv://ARYANJ:Z3cdDHiFGxiTMEmy@blogdb.iui6lvp.mongodb.net/MyDB?retryWrites=true&w=majority")
 .then(()=>app.listen(9000))
 .then(()=>console.log("Connected to mongoDB"))
