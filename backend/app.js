eeccfd25c0a3b429f9d940ea967f10f748ec45a1
ARYANNJ <aryanjadhav71@gmail.com>
2023-07-21 19:37:04 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 const app=express();
 
 mongoose.connect("mongodb+srv://ARYANJ:Z3cdDHiFGxiTMEmy@blogdb.iui6lvp.mongodb.net/MyDB?retryWrites=true&w=majority")
-.then(()=>app.listen(8000))
+.then(()=>app.listen(9000))
 .then(()=>console.log("Connected to mongoDB"))
 .catch((err)=>console.log(err));
