6a088280a13411061abece3484d4b46586befa18
ARYANNJ <aryanjadhav71@gmail.com>
2023-07-21 21:37:22 +0530

    Blog creation done

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
 import router from "./routes/user.routes";
 const app=express();
 app.use(express.json());
-
 app.use("/api/user",router);
+app.use("/api/blog",blogRouter);
 mongoose.connect("mongodb+srv://ARYANJ:Z3cdDHiFGxiTMEmy@blogdb.iui6lvp.mongodb.net/MyDB?retryWrites=true&w=majority")
 .then(()=>app.listen(9000))
 .then(()=>console.log("Connected to mongoDB"))
